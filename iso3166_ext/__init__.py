0eb2ebd660eb4d179584ebcaa78b411bdbb00d1d
Martin Hvidberg <Martin@Hvidberg.net>
2020-03-31 16:55:43 +0200

    ver. 0.0.3 - Import is smooth, getting ready for compatible - SEQ202003311655

--- iso3166_ext/__init__.py
+++ iso3166_ext/__init__.py
@@ -7,7 +7,7 @@
 import os  # Needed for definition of root_dir
 from iso3166_ext.world import *  # import this module on package import
 
-__version__ = "0.0.2"  # Version of the iso3166_ext package
+__version__ = "0.0.3"  # Version of the iso3166_ext package
 __all__ = ['world']  # import these modules on package import *
 
 # The root path of this package, for later reference, I think ...
